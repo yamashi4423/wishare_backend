9cd5e194b4c6e00c732211d3546f755f198f7ae4
yamashi4423 <yamashi4423@gmail.com>
2024-03-22 04:51:16 +0900

    シリアライザ、ビュー、URLルーティングを作成

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# ViewSet: APIのクエリーをどう解釈するかを決めるもの
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import filters, viewsets
+
+from .models import Family, Product, User
+from .serializers import FamilySerializer, ProductSerializer, UserSerializer
+
+
+class FamilyViewSet(viewsets.ModelViewSet):
+    queryset = Family.objects.all()
+    serializer_class = FamilySerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
